28260c6cf1cb2db1bd60f249b20738a529de6027
Lovepreet Singh <lovepreet.singh@trantorchd.com>
2021-04-26 12:04:32 +0530

    refactored code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./app.scss";
+import FormikControl from "./elements/Formik/FormikControl";
+import Checkbox from "./elements/Formik/Checkbox";
+import DatePicker from "./elements/Formik/DatePicker";
+import FieldArrayComponent from "./elements/Formik/FieldArray"
+import CustomField from "./elements/Formik/CustomField";
+import Label from "./elements/Formik/Label";
+import CollapsableSection from "./elements/UI/CollapseableSection/CollapseableSection";
+import RoundedEdgesCard from "./elements/UI/RoundedEdgesCard/RoundedEdgesCard";
+import Stepper from "./elements/UI/Stepper/Stepper";
+import UncontrolledDropdown from "./elements/UI/UncontrolledDropdown/UncontrolledDropdown";
+import ButtonComponent from "./elements/UI/ButtonComponent";
+import ModalComponent from "./elements/UI/ModalComponent";
+import Note from "./elements/UI/Note";
+export { FormikControl, Checkbox, DatePicker, FieldArrayComponent, CustomField, Label, CollapsableSection, RoundedEdgesCard, Stepper, UncontrolledDropdown, ButtonComponent, ModalComponent, Note };
